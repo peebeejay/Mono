aebf07a4af07a7c63d98453bda091deecec54141
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2017-05-02 18:17:52 -0400

    complete integration test

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -7,44 +7,39 @@ require 'byebug'
 class ControllerBase
   attr_reader :req, :res, :params
 
-  # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, params = {})
     @req = req
     @res = res
+    @params = @req.params.merge(params)
     @already_built_response = false
   end
 
-  # Helper method to alias @already_built_response
   def already_built_response?
     @already_built_response == true
   end
 
-  # Set the response status code and header
   def redirect_to(url)
     if already_built_response?
       raise "Error - Double Response"
     else
       @res.set_header("Location", url)
       @res.status = 302
+      session.store_session(@res)
       @already_built_response = true
     end
   end
 
-  # Populate the response with content.
-  # Set the response's content type to the given type.d
-  # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
     if already_built_response?
       raise "Error - Double Response"
     else
       @res.body = [content]
       @res['Content-Type'] = content_type
+      session.store_session(@res)
       @already_built_response = true
     end
   end
 
-  # use ERB and binding to evaluate templates
-  # pass the rendered html to render_content
   def render(template_name)
     if already_built_response?
       raise "Error - Double Response"
@@ -57,11 +52,11 @@ class ControllerBase
     end
   end
 
-  # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
-  # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
   end
 end
