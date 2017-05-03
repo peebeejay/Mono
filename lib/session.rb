c21236ba3cb2a068755b894249041b8a3bbafebe
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2017-05-02 21:15:33 -0400

    Initial Commit

--- lib/session.rb
+++ lib/session.rb
@@ -1,8 +1,6 @@
 require 'json'
 
 class Session
-  # find the cookie for this app
-  # deserialize the cookie into a hash
   def initialize(req)
     @session_cookie = req.cookies["_rails_lite_app"] ? JSON.parse(req.cookies["_rails_lite_app"]) : {}
   end
@@ -15,8 +13,6 @@ class Session
     @session_cookie[key] = val
   end
 
-  # serialize the hash into json and save in a cookie
-  # add to the responses cookies
   def store_session(res)
     res.set_cookie('_rails_lite_app', path: '/', value: @session_cookie.to_json)
   end
